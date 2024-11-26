41058de9cfdd8e888a5d0ba1db8ebae392c70467
pooky <157203163+pookyhyeon@users.noreply.github.com>
2024-11-26 20:30:21 +0900

    dd
    d

--- script.js
+++ script.js
@@ -2,8 +2,9 @@ document.addEventListener("DOMContentLoaded", () => {
     const intro = document.getElementById("intro");
     const menu = document.getElementById("menu");
 
+    // 3초 후 인트로 메시지 사라짐
     setTimeout(() => {
-        intro.classList.add("hidden");
-        menu.style.display = "block";
-    }, 8000); // 5초 후 메뉴판 표시
+        intro.classList.add("hidden"); // 인트로 숨기기
+        menu.classList.remove("hidden"); // 메뉴판과 공지 표시
+    }, 7000); // 3초 후 실행
 });
